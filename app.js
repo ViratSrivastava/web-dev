a29e7606651d22a0edd5540337250fc192edcf2e
Virat Srivastava <vs.viratsrivastava11111@gmail.com>
2024-09-23 11:43:44 +0530

    idk what I'm Doing

---
+++ app.js
@@ -0,0 +1,57 @@
+// is used for comments
+/*
+comments for paragraphs
+*/
+console.log("hello world");
+// console.log is a function that logs a message to the console
+
+// datatypes: undefiend, null, boolean, string, synmbol, number and object
+// var is used to declare a variable
+
+var number = 10; 
+// declaring a variable
+console.log(number);
+// console.log(number) is used to log the value of the variable number to the console
+var number = 19;
+console.log(number);
+// variables can be reassigned
+
+let dob = 11/10/2002;
+console.log(dob);
+// let is used to declare a variable
+
+const pi = 3.14;
+console.log(pi);
+// const is used to declare a variable that cannot be reassigned
+
+ var a;
+    console.log(a);
+    // a is undefined
+    a = 5;
+    console.log(a);
+    // a is 5
+
+var b = 2;
+    console.log(b);
+    // b is 2
+    b = a;
+    console.log(b);
+    // b is 5
+
+//initailizing using Assignment operator
+
+var a = 9;
+var b = 10;
+var c = "This my variable assigment";
+
+a = a + 2;
+b = b + 4;
+c = c + "and I'm using Assignment operator";
+console.log(a);
+console.log(b);
+console.log(c); 
+
+// Variables are case Sensitive
+var Name = "Virat Srivastava";
+var name = "Virat";
+
